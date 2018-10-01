1f84d84c8f820637abe9744f6cb4c9009035cf39
fedeatanasoff <fede.atanasoff@gmail.com>
2018-10-01 10:09:41 -0300

    modificando favicon y agregando imagenes a assets

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
