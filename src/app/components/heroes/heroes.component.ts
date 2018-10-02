37c05879006f84f1ad0c9e1f3d9a8091a87c1e19
fedeatanasoff <fede.atanasoff@gmail.com>
2018-10-01 22:15:55 -0300

    mostrando heroes y linkeando la ruta del detalle

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HeroesService, HeroeInterface } from "../../servicios/heroes.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-heroes",
@@ -9,10 +10,15 @@ import { HeroesService, HeroeInterface } from "../../servicios/heroes.service";
 export class HeroesComponent implements OnInit {
   heroes: any[] = [];
 
-  constructor(private _heroesService: HeroesService) {}
+  constructor(private _heroesService: HeroesService, private _route: Router) {}
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe(id: number) {
+    // console.log(id);
+    this._route.navigate(["/heroe", id]);
   }
 }
