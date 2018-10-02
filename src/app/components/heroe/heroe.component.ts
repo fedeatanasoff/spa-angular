3b2a80e723d94bf5a1006c210754d8e028fdd811
fedeatanasoff <fede.atanasoff@gmail.com>
2018-10-02 00:05:25 -0300

    mostrando info de heroe en HeroeComponent

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { HeroesService } from "../../servicios/heroes.service";
 
 @Component({
@@ -11,12 +11,17 @@ export class HeroeComponent {
   heroe: any = {};
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _heroeService: HeroesService
+    private _heroeService: HeroesService,
+    private _router: Router
   ) {
     this._activatedRoute.params.subscribe(params => {
       this.heroe = this._heroeService.getHeroe(params["id"]);
       console.log(this.heroe);
     });
+  }
+
+  regresarHeroes() {
+    this._router.navigate(["/about"]);
   }
 
   // ngOnInit() {}
