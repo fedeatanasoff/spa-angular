c204c3881a1a93c4e7aef043475d74cc468102a9
fedeatanasoff <fede.atanasoff@gmail.com>
2018-10-06 01:45:41 -0300

    corrigiendo funcionalidad de ver mas en heroe component con output y eventEmitter

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
@@ -12,12 +12,19 @@ export class HeroeTarjetaComponent implements OnInit {
 
   @Input()
   indice: number;
-  constructor(private _router: Router) {}
+
+  @Output()
+  heroeSeleccionado: EventEmitter<number>;
+
+  constructor(private _router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit() {}
 
   verHeroe() {
-    console.log(this.indice);
-    this._router.navigate(["/heroe", this.indice]);
+    // console.log(this.indice);
+    // this._router.navigate(["/heroe", this.indice]);
+    this.heroeSeleccionado.emit(this.indice);
   }
 }
